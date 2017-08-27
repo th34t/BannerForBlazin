431b3f6fd2f9b6e31c01d4ef9907d788e2a26232
th34t <31392934+th34t@users.noreply.github.com>
2017-08-27 22:41:31 +0100

    Add files via upload

---
+++ Startup.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os, sys, time
+from tc import *
+
+def Main():
+	print color.R+"		+==================================================================================+ "
+	time.sleep(0.07)
+	print color.R+"		|   ____   _              _           _                      _                     | "
+	time.sleep(0.07)
+	print color.R+"		|  |  _ \ | |            (_)         | |                    | |                    | "
+	time.sleep(0.07)
+	print color.R+"		|  | |_) || |  __ _  ____ _  _ __    | |_  _ __  __ _   ___ | | __ ___  _ __  ___  | "
+	time.sleep(0.07)
+	print color.G+"		|  |  _ < | | / _` ||_  /| || '_ \   | __|| '__|/ _` | / __|| |/ // _ \| '__|/ __| | "
+	time.sleep(0.07)
+	print color.G+"		|  | |_) || || (_| | / / | || | | | _| |_ | |  | (_| || (__ |   <|  __/| |   \__ \ | "
+	time.sleep(0.07)
+	print color.G+"		|  |____/ |_| \__,_|/___||_||_| |_|(_)\__||_|   \__,_| \___||_|\_\\ ___||_|   |___/ | "
+	time.sleep(0.07)
+	print color.G+"		+==================================================================================+ "
+	time.sleep(0.07)
+	print color.G+"		Instagram: @blazin.trackerz Twitter: @wowimturtle. Currently working on a programme  "
+	time.sleep(0.07)
+	print color.G+"		called spank.exe                                                                     "
+
+Main()
